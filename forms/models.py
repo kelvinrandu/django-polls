ad91ca4b6b43c21469aacdbbcefb2d5772ea15a1
kelvinrandu <randukelvin@gmail.com>
2022-03-06 10:50:45 +0300

    added models

--- forms/models.py
+++ forms/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 # Create your models here.
 class Form(models.Model):
-    name= models.TextField()
-    email=models.TextField()
+    name= models.CharField(max_length=150, blank=False)
+ 
+    email = models.EmailField(('email'), blank=True, unique=True,
+                                        help_text='254 characters or fewer. Must be a valid email address.')
     address=models.TextField()
-    computer_literate=models.TextField()
+    computer_literate=models.BooleanField(('active'), default=True)
     income=models.TextField()
+    years_of_experience = models.PositiveSmallIntegerField(blank=True, null=True)
--- polls/settings.py
+++ polls/settings.py
@@ -9,22 +9,31 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ,os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True),
+    SECRET_KEY =(str),
+)
+# environ.Env.read_env()
+
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@8i4y&h)fh#zw-tuxyfgk1o$8@s0!-i$ne_y7hy_2^x!+xixd_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = []
 
 
