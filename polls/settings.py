ecef0ace11baf60ac8c393253dc3e62860f5fc3f
kelvinrandu <randukelvin@gmail.com>
2022-03-07 21:15:37 +0300

    editted settings.py

--- polls/settings.py
+++ polls/settings.py
@@ -147,6 +147,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 django_heroku.settings(locals())
