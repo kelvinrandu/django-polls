dc9dc4914d71e8514d80ad1424dcc58280ec69b0
kelvinrandu <randukelvin@gmail.com>
2022-03-07 21:11:40 +0300

    editted settings.py

--- polls/settings.py
+++ polls/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
