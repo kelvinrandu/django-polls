2114a162e77bfafb8d65f659688b2c1582d483cb
kelvinrandu <randukelvin@gmail.com>
2022-03-08 15:26:09 +0300

    added create persona views

--- forms/urls.py
+++ forms/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add/', views.get_name, name='add'),
-     path("create/", views.CreatePersona.as_view(), name="create"),
+    path("create/", views.CreatePersona.as_view(),name="create"),
+  
 ]
--- forms/views.py
+++ forms/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 from django.views.generic import CreateView
 from .forms import FormForm
 from django.views import generic
-from django.core.mail import send_mail
+from django.urls import reverse_lazy
 
 from .models import Form
 
@@ -12,44 +12,13 @@ from .models import Form
 class CreatePersona(generic.FormView):
     model = Form
     form_class = FormForm
+    success_url = '/forms/create'
+
     template_name = 'forms/create.html'
+    # context = {
+    #     'form': form_class,
+    # }
 
-
-def get_name(request):
-    template = loader.get_template('forms/create.html')
-    context = {
-        'response_list': 'response',
-    }
-
-    
-
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = FormForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            subject = form.cleaned_data['name']
-            message = form.cleaned_data['address']
-            sender = form.cleaned_data['email']
-            recipients = ['info@example.com']
-
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            send_mail(subject, message, sender, recipients)
-            return HttpResponseRedirect('/forms')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = FormForm()
-        response = Form.objects.all()
-        context = {
-        'response_list': response,
-    }
-
-
-    return HttpResponse(template.render(context, request))
 
 # # Create your views here.
 def index(request):
